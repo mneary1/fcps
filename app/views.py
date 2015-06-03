8748898908d72c5b7ed5c6135dcfbf9093803089
Michael Neary <mneary1@umbc.edu>
2015-06-02 22:50:46 -0400

    login form

--- app/views.py
+++ app/views.py
@@ -1,7 +1,16 @@
-from flask import render_template 
+from flask import render_template, flash, redirect 
+from .forms import LoginForm
 from app import app
 
 @app.route('/')
+@app.route('/home')
 def index():
-    return render_template("base.html",body="Welcome to FCPS at CTY 2015.",\
-                           name="Michael")
+    return render_template("base.html")
+
+@app.route('/login',methods=['GET','POST'])
+def sign_in():
+	form = LoginForm()
+	if form.validate_on_submit():
+		flash('Login requested for user: %s pass: %s ' % (form.username.data, form.password.data) )
+		return redirect('/')
+	return render_template("login.html", form=form)
