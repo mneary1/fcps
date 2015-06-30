2f772a785734f4754115be3a80b8e73ffafb222c
Michael Neary <mpneary5@gmail.com>
2015-06-30 17:52:27 -0400

    handles error when uploading file with no extension

--- app/views.py
+++ app/views.py
@@ -69,7 +69,7 @@ def upload():
     '''
     if request.method == 'POST':
         filename = secure_filename(form.upload.data.filename)
-        if filename:
+        if filename and "." in filename:
             ext = filename.split(".")[1]
             if ext not in app.config["ALLOWED_EXTENSIONS"]:
                 flash("file extension ." + ext + " is not a supported upload type", "error")
