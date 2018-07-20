9e2b7de31b6a73830aea709c3b4f5f10a29664f0
Michael Neary <mneary1@umbc.edu>
2018-07-20 04:13:25 +0000

    works with new versions of flask-login, and fixes model.py tabs/spaces issue

--- app/models.py
+++ app/models.py
@@ -1,30 +1,28 @@
 from app import db
 
 class User(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String(64), index=True, unique=True)
-	firstname = db.Column(db.String(64), unique=False)
+        id = db.Column(db.Integer, primary_key=True)
+        username = db.Column(db.String(64), index=True, unique=True)
+        firstname = db.Column(db.String(64), unique=False)
         lastname = db.Column(db.String(64), unique=False)
         password = db.Column(db.String(64), index=True, unique=False)
         session = db.Column(db.Integer)
         year = db.Column(db.String(4), unique=False)
 
-	def __repr__(self):
-		return "User<%r> Name<%r, %r> Year<%r> Session<%r>" % (self.username, self.lastname, self.firstname, self.year, self.session)
+        def __repr__(self):
+                return "User<%r> Name<%r, %r> Year<%r> Session<%r>" % (self.username, self.lastname, self.firstname, self.year, self.session)
 
         def __str__(self):
                 return "%s, %s %s session %d" % (self.lastname, self.firstname, self.year, self.session)
 
-	def is_authenticated(self):
-		return True
+        def is_authenticated(self):
+                return True
 
-	def is_active(self):
-		return True
+        def is_active(self):
+                return True
 
-	def is_anonymous(self):
-		return False
+        def is_anonymous(self):
+                return False
 
-	def get_id(self):
-		return str(self.id)
-
-	
+        def get_id(self):
+                return str(self.id)
